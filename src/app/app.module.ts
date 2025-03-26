ec8353cd3c8ea91ada3f539602cbe1abca68b375
PrateekSharma007 <btech.m2_66995@apsc.mait.ac.in>
2025-03-26 23:20:54 +0530

    structure updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './basic/components/login/login.component';
+import { SignupComponent } from './basic/components/signup/signup.component';
+import { DemoNgZorroAntModule } from './DemoNgZorroAntModule';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SignupComponent
+  ],
   imports: [
     CommonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DemoNgZorroAntModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ]
 })
 export class AppModule { }
---
+++ src/app/company/pages/companydashboard/companydashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-companydashboard',
+  imports: [],
+  templateUrl: './companydashboard.component.html',
+  styleUrl: './companydashboard.component.scss'
+})
+export class CompanydashboardComponent {
+
+}
